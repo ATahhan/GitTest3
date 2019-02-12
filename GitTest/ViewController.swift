7e85a3d62dca5e6aec0755b7134bb5e48ad383bc
Ammar Tahhan <amr.tahhan@gmail.com>
2019-02-12 20:10:59 +0300

    Initial Commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Ammar AlTahhan on 12/02/2019.
+//  Copyright © 2019 Ammar AlTahhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
