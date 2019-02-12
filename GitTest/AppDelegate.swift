75b559923e2f3fabc9d45904be0c066838871eba
Ammar Tahhan <amr.tahhan@gmail.com>
2019-02-12 20:14:52 +0300

    New feature

--- GitTest/AppDelegate.swift
+++ GitTest/AppDelegate.swift
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         print("Printing")
+        print("New_feature")
         
         return true
     }
